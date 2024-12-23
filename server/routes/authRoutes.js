123cea624c5649b0eebf919b0508fab50853acd1
RJLarot <1801101934@buksu.edu.ph>
2024-12-23 10:07:08 +0800

    sorry late upload

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,3 +7,9 @@ const router = express.Router();
 router.post('/google-login', verifyGoogleToken);
 
 module.exports = router;
+//GOOGLE_CLIENT_ID=295099131481-l6mgeh805cerlvbav583lb8tuqm4trrb.apps.googleusercontent.com
+//GOOGLE_CLIENT_SECRET=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
+//JWT_SECRET=i_love_pingpong
+
+//6LfYa4YqAAAAACwKqbwlXqecTZsyjFY2eG_-KZ_Q
+//6LfYa4YqAAAAAOFvZRnLc0s7zzauZw-53m4OJuoz
